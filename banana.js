f823c71b77738a6de3cd025ca40c33fa019846f3
sharpMW <88919653+sharpMW@users.noreply.github.com>
2021-08-17 12:07:53 +0530

    final update

--- banana.js
+++ banana.js
@@ -1,23 +1,26 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var textInput = document.querySelector("#txt-input");
-console.log(textInput);
-var output = document.querySelector("#output");
-var serverURL = "https://api.funtranslations.com/translate/yoda.json";
-function getTranslationURL(text) { 
-    return serverURL + "?" + "text" + text 
-}
-btnTranslate.addEventListener("click", clickHandler);
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/pirate.json";
 
-// function errorHandler(error) { 
-//     console.log("Error occured", error);
-//     alert("Something went wrong");
-// }
+function getTranslationURL(input) { 
+    return serverURL + "?" + "text=" + input
+}
+
+
+function errorHandler(error) { 
+    console.log("Error occured", error);
+    alert("Something went wrong")
+}
 function clickHandler() {
-   var inputText = textInput.value;
+   var inputText = txtInput.value;
+
     fetch(getTranslationURL(inputText))
    .then(response => response.json())
    .then(json => {
-       var outputText = json.contents.translated;
-       output.innertext = outputText;})
-//    .catch(errorHandler)
-};
+       var translatedText = json.contents.translated;
+       outputDiv.innertext = translatedText;})
+   .catch(errorHandler)
+   };
+   btnTranslate.addEventListener("click", clickHandler);
